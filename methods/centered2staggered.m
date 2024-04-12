641695a838ba41b8506e5d6994bb7d0cb40c1f91
spiggen <jonasvilgotlotberg@hotmail.se>
2024-04-12 10:59:34 +0200

    :3

--- methods/centered2staggered.m
+++
@@ -1,5 +0,0 @@
-v_x(:,2:end) = (v_x_centered(:,1:end-1).*is_fluid(:,1:end-1) + v_x_centered(:,2:end).*is_fluid(:,2:end))./2;
-v_x(:,1)     =  v_x_centered(:,1);
-
-v_y(2:end,:) = (v_y_centered(1:end-1,:).*is_fluid(1:end-1,:) + v_y_centered(2:end,:).*is_fluid(2:end,:))./2;
-v_y(1,:)     =  v_y_centered(1,:);
